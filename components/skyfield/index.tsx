341c768538116bbf5e7c62572fe6ce02a9618486
root <keithohno@gmail.com>
2022-06-06 15:59:09 -0400

    added star color and magnitude
    Modified the frag shader to create a "glow" effect and added color
    (using Wikipedia's stellar classifications as a reference).

--- components/skyfield/index.tsx
+++ components/skyfield/index.tsx
@@ -9,13 +9,48 @@ const initBuffers = async (gl: WebGLRenderingContext) => {
   // fetch binary data
   let positionData: number[] = [];
   let sizeData: number[] = [];
+  let colorData: number[] = [];
   const res = await (await (await fetch("BSC5ra-small")).blob()).arrayBuffer();
   for (let i = 0; i < res.byteLength; i += 19) {
     let floats = new Float64Array(res.slice(i, i + 16));
     positionData.push(RADIUS * Math.cos(floats[0]) * Math.cos(floats[1]));
     positionData.push(RADIUS * Math.sin(floats[0]) * Math.cos(floats[1]));
     positionData.push(RADIUS * Math.sin(floats[1]));
-    sizeData.push(5);
+    let sizes = new Int16Array(res.slice(i + 16, i + 18));
+    sizeData.push((sizes[0] - 610) / 6);
+    // color by stellar classification
+    switch (new Uint8Array(res.slice(i + 18, i + 19))[0]) {
+      case 77:
+        colorData.push(1.0, 0.5, 0.3);
+        break;
+      case 75:
+        colorData.push(1.0, 0.7, 0.4);
+        break;
+      case 71:
+        colorData.push(1.0, 0.8, 0.7);
+        break;
+      case 70:
+        colorData.push(0.9, 0.9, 0.9);
+        break;
+      case 65:
+        colorData.push(0.7, 0.8, 1.0);
+        break;
+      case 66:
+        colorData.push(0.4, 0.7, 1.0);
+        break;
+      case 79:
+        colorData.push(0.2, 0.4, 1.0);
+        break;
+      case 83:
+      case 67:
+        colorData.push(1.0, 0.3, 0.2);
+        break;
+      case 87:
+        colorData.push(1.0, 1.0, 1.0);
+        break;
+      default:
+        colorData.push(0.9, 0.9, 0.9);
+    }
   }
 
   // load data into buffers
@@ -29,16 +64,19 @@ const initBuffers = async (gl: WebGLRenderingContext) => {
   const sizes = gl.createBuffer()!;
   gl.bindBuffer(gl.ARRAY_BUFFER, sizes);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sizeData), gl.STATIC_DRAW);
-  return { positions, sizes };
+  const colors = gl.createBuffer()!;
+  gl.bindBuffer(gl.ARRAY_BUFFER, colors);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+  return { positions, sizes, colors };
 };
 
 function drawScene(
   gl: WebGLRenderingContext,
   uniforms: any,
-  buffer: { positions: WebGLBuffer; sizes: WebGLBuffer },
+  buffer: { positions: WebGLBuffer; sizes: WebGLBuffer; colors: WebGLBuffer },
   orientation: mat4
 ) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.0, 0.02, 0.04, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // perspective matrix
@@ -66,6 +104,11 @@ function drawScene(
   gl.vertexAttribPointer(1, 1, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(1);
 
+  // format of size buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer.colors);
+  gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(2);
+
   // draw
   gl.drawArrays(gl.POINTS, 0, 2000);
 }
@@ -76,6 +119,7 @@ const Skyfield: React.FC = () => {
   const [buffers, setBuffers] = useState<{
     positions: WebGLBuffer;
     sizes: WebGLBuffer;
+    colors: WebGLBuffer;
   }>();
 
   const [rot1, setRot1] = useState<vec3>(vec3.fromValues(0, 1, 0));
@@ -158,6 +202,9 @@ const Skyfield: React.FC = () => {
     const vsSource = `
       attribute vec4 aPosition;
       attribute float aSize;
+      attribute vec3 aColor;
+      
+      varying lowp vec3 vColor;
   
       uniform mat4 uModel;
       uniform mat4 uView;
@@ -166,17 +213,21 @@ const Skyfield: React.FC = () => {
       void main() {
         gl_Position = uProjection * uModel * uView * aPosition;
         gl_PointSize = aSize;
+        vColor = aColor;
       }
     `;
     const fsSource = `
       precision lowp float;
 
+      varying lowp vec3 vColor;
+
       void main() {
         vec2 xy_norm = 2.0 * gl_PointCoord - 1.0;
-        if (dot(xy_norm, xy_norm) > 1.0) {
-            discard;
+        float r2 = dot(xy_norm, xy_norm);
+        if (r2 > 1.0) {
+          discard;
         }
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 0.6);
+        gl_FragColor = vec4(vColor, 0.7 / (r2 * 45.0 + 0.5));
       }
     `;
 
