fecd250951646f725872ba2c6beca70facedb901
root <keithohno@gmail.com>
2022-06-07 23:40:17 -0400

    added animated tabs

--- components/main/tabs.tsx
+++ components/main/tabs.tsx
@@ -1,43 +1,53 @@
-import styled from "@emotion/styled";
+import { useState } from "react";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
-const Tabs: React.FC = () => {
+const Tab: React.FC<{ src: string; scrollTo: string; tabText: string }> = (
+  props
+) => {
+  const [hovered, setHovered] = useState(false);
+
   const scrollFn = (id: string) => {
     document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      css={tab}
+    >
+      <div css={expandableouter}>
+        <div
+          css={expandableinner}
+          onClick={() => {
+            scrollFn(props.scrollTo);
+          }}
+          style={{ marginLeft: hovered ? "0" : "-100%" }}
+        >
+          {props.tabText}
+        </div>
+      </div>
+      <img
+        css={icon}
+        src={props.src}
+        onClick={() => {
+          scrollFn(props.scrollTo);
+        }}
+      ></img>
+    </div>
+  );
+};
+
+const Tabs: React.FC = () => {
+  return (
     <Main>
       <TabArray>
-        <img
-          css={[Tab]}
-          src="moon.png"
-          onClick={() => {
-            scrollFn("titlesection");
-          }}
-        ></img>
-        <img
-          css={[Tab]}
-          src="earth.png"
-          onClick={() => {
-            scrollFn("projectsection");
-          }}
-        ></img>
-        <img
-          css={[Tab]}
-          src="stars.png"
-          onClick={() => {
-            scrollFn("starsection");
-          }}
-        ></img>
-        <img
-          css={[Tab]}
-          src="mars.png"
-          onClick={() => {
-            scrollFn("contactsection");
-          }}
-        ></img>
+        <Tab src="moon.png" scrollTo="titlesection" tabText="Bio" />
+        <Tab src="earth.png" scrollTo="projectsection" tabText="Projects" />
+        <Tab src="stars.png" scrollTo="starsection" tabText="Stars" />
+        <Tab src="mars.png" scrollTo="contactsection" tabText="Contact" />
       </TabArray>
     </Main>
   );
@@ -50,7 +60,22 @@ const Main = styled.div`
   justify-content: center;
 `;
 
-const Tab = css`
+const TabArray = styled.div`
+  display: flex;
+  justify-content: space-around;
+  height: calc(400px + 8vw);
+  width: calc(60px + 4vw);
+  flex-direction: column;
+`;
+
+const tab = css`
+  position: relative;
+  height: 80px;
+  padding: 10px;
+`;
+
+const icon = css`
+  position: absolute;
   width: 60px;
   height: 60px;
   border-radius: 50%;
@@ -69,12 +94,29 @@ const Tab = css`
   }
 `;
 
-const TabArray = styled.div`
+const expandableouter = css`
+  position: absolute;
+  left: 40px;
+  overflow: hidden;
+`;
+
+const expandableinner = css`
+  position: relative;
+  width: 100%;
+  height: 60px;
+  padding-left: 50px;
+  padding-right: 15px;
+
   display: flex;
-  justify-content: space-around;
-  height: calc(400px + 8vw);
-  width: calc(60px + 4vw);
-  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  overflow: hidden;
+  transition: all 0.5s;
+
+  border-radius: 0px 30px 30px 0px;
+  background-color: rgb(255, 255, 255, 0.2);
+  color: #cacac9;
+  font-size: 20;
 `;
 
 export default Tabs;
