1a636ef29f048978975fe41287ecbd72ac718a89
Keith <keithohno@gmail.com>
2023-03-24 18:22:58 -0400

    Build project

--- out/_next/static/VycmUKLzr0KwKQRI4yo3Z/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/169-4488701a1349e257.js","static/chunks/pages/index-68ba11d7480c749b.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ out/_next/static/e8ppp0E7Fo8VZupg2QJgO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/519-198b35fb76240a2b.js","static/chunks/pages/index-9af76399f7470962.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
