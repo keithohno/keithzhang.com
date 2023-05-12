2cb18b5b85184e735ed62c49381bcabdf20aa871
Keith <keithohno@gmail.com>
2023-05-11 22:02:26 -0400

    KZ-2: added icon buttons

--- features/main/v2/TitleSection.tsx
+++ features/main/v2/TitleSection.tsx
@@ -15,11 +15,43 @@ const TitleSection: React.FC = () => {
             seeking new opportunities to solve complex problems, develop
             meaningful connections, and impact the world at large.
           </MedText>
+          <HFlexIcons>
+            <a href="https://www.github.com/keithohno">
+              <IconButton src="github.svg" alt="github" />
+            </a>
+            <a href="https://www.linkedin.com/in/keith-zhang">
+              <IconButton src="linkedin.svg" alt="linkedin" />
+            </a>
+          </HFlexIcons>
         </VFlex>
       </HFlex>
     </PageSectionV2>
   );
 };
+
+const HFlexIcons = styled.div`
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+  @media (min-width: 600px) {
+    padding-right: 4rem;
+  }
+`;
+
+const IconButton = styled.img`
+  box-sizing: content-box;
+  width: 2rem;
+  height: 2rem;
+  padding-bottom: 4px;
+  border-bottom: 2px solid rgb(0, 0, 0, 0);
+  transition: all 0.2s ease-in-out;
+  :hover {
+    transform: scale(1.1);
+    border-bottom: 2px solid #fed;
+  }
+`;
 
 const HFlex = styled.div`
   width: 95%;
