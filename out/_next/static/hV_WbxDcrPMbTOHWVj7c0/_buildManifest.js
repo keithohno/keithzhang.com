6bcad8953e4ccd66bcd436bc91f9ae7033e1b861
unknown <keithohno@gmail.com>
2024-01-10 21:15:18 -0500

    KZ-337: v3 deployment

---
+++ out/_next/static/hV_WbxDcrPMbTOHWVj7c0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/519-064c3c1c9e7a625e.js","static/chunks/pages/index-4d4f3ea2ac9ec55d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
